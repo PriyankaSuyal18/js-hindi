1cde1dd7a2ab9c7c5f698c3ca2a12d7bf66259ef
Priyanka Suyal <158487903+PriyankaSuyal18@users.noreply.github.com>
2025-01-23 17:40:58 +0000

    datatypes

---
+++ 01_var.js
@@ -0,0 +1,11 @@
+const accountId = 12247;
+let accountEmail="priyanka@gmail.com";
+var accountPassword = "23232";
+accountName = "hdfc";
+
+// accountId = 898;  // Assignment to constant variable.not allowed 
+accountEmail = "pri@google.com";
+accountPassword = "998";
+accountName = "icci";
+
+console.table([accountId,accountEmail,accountPassword,accountName]);
